5f5e06e810525b1db1e9a1a3ee761ceb86690565
TyraVex <thomas.baruzier@gmail.com>
2022-05-26 14:03:42 +0200

    Updates

--- archives/pas-a-pas/script.js
+++ archives/pas-a-pas/script.js
@@ -25,8 +25,9 @@ gallery.querySelectorAll('img').forEach(function (item) {
     }
 });
 window.addEventListener('resize', resizeAll);
-gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    item.addEventListener('click', function () {        
-        item.classList.toggle('full');        
-    });
-});
+//gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+//    item.addEventListener('click', function () {
+//        item.classList.toggle('full');
+//    });
+//});
+
